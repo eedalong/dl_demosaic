c8b7a8266fa28f7b8b790c794f78080ea418355b
chson0316 <32611764+chson0316@users.noreply.github.com>
2017-12-03 23:17:39 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -41,3 +41,29 @@ def add_noise(data, sigma):
 
     return noisy_data
 
+def remosaic(i_rgb):
+    batch_size, ch, patch_height, patch_width = i_rgb.shape
+    bayer = torch.zeros(batch_size, patch_height, patch_width)
+
+    bayer[:,  ::2,  ::2] = i_rgb[:, 1,  ::2,  ::2]  # G
+    bayer[:,  ::2, 1::2] = i_rgb[:, 0,  ::2, 1::2]  # R
+    bayer[:, 1::2,  ::2] = i_rgb[:, 2, 1::2,  ::2]  # B
+    bayer[:, 1::2, 1::2] = i_rgb[:, 1, 1::2, 1::2]  # G
+
+    bayer_rgb = torch.zeros(batch_size, 3, patch_height, patch_width)
+
+    bayer_rgb[:, 1,  ::2,  ::2] = bayer[:,  ::2,  ::2]  # G
+    bayer_rgb[:, 0,  ::2, 1::2] = bayer[:,  ::2, 1::2]  # R
+    bayer_rgb[:, 2, 1::2,  ::2] = bayer[:, 1::2,  ::2]  # B
+    bayer_rgb[:, 1, 1::2, 1::2] = bayer[:, 1::2, 1::2]  # G
+
+    return bayer_rgb
+
+
+import torchvision
+def unnormalize(images):
+    img = torchvision.utils.make_grid(images)
+    img   = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    npimg = np.transpose(npimg, (1, 2, 0))
+    return npimg
